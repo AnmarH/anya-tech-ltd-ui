74e727cc414d782ec06191d85869b064e1f7650b
Anmar Hammadi <anmar.hammadi@outlook.com>
2024-04-16 17:33:00 +0100

    testing CICD with component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'anya-tech-ltd-ui';
+  title = 'anya-tech-ltd-ui-TESTTESaasdasdT';
 }
