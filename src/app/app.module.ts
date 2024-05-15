207d8b76803aad7602186246d468917172fc3a3c
Anmar Hammadi <anmar.hammadi@outlook.com>
2024-05-15 19:45:32 +0100

    added new high level empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ContractorsComponent } from './contractors/contractors.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContractorsComponent,
+    ContactUsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
